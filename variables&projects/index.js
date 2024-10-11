d65411a9f5b092948d33e5bb3608e41fc0dfc01f
Aniket-nitian <anik807621@gmail.com>
2024-10-11 15:30:40 +0530

    New repo created for js notes

---
+++ variables&projects/index.js
@@ -0,0 +1,10 @@
+
+console.log("Best of Luck");
+console.error("it's a false error");
+console.warn("It's a false warning");
+let message='jaagrata';
+    console.log(message);
+    let quantity=0;
+const myName='Aniket chauhan';
+console.log(myName);
+document.querySelector('p').innerText= `My Name is ${myName}`
