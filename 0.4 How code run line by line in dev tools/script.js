4fb277758b418f05809fd911a7c5ac6b6beb7989
Aniket-nitian <anik807621@gmail.com>
2024-10-12 14:36:29 +0530

    Code execution completed

---
+++ 0.4 How code run line by line in dev tools/script.js
@@ -0,0 +1,12 @@
+let firstName = 'Aniket'
+let age = 21
+let yearofBirth = 2023
+let isHappy = true
+
+console.log(firstName)
+// let userIntro = 'Hi! my  name is ' + firstName + 'and  I am ' + age +'years old.'
+
+// console.log(userIntro)
+// firstName = 'Chauhan'
+
+// const  cast = 'Rajput'
