80a07bcbb37f2997a5132e2c852d2a85960ee783
Aniket-nitian <anik807621@gmail.com>
2024-10-12 13:25:54 +0530

    Variable completed

---
+++ 0.3 Variable/script.js
@@ -0,0 +1,10 @@
+let firstName = 'Aniket'
+let age = 21
+let isHappy = true
+
+let userIntro = 'Hi! my  name is ' + firstName + 'and  I am ' + age +'years old.'
+
+console.log(userIntro)
+firstName = 'Chauhan'
+
+const  cast = 'Rajput'
