09e4e361e6467d1f5524d84d7b1c00ffc3a9e87b
Aniket-nitian <anik807621@gmail.com>
2024-10-12 15:53:47 +0530

    Pop up completed

---
+++ 0.5 Dialog boxes/script.js
@@ -0,0 +1,8 @@
+alert("hello welcome to my github")
+confirm("Are you good or not")
+
+let isStudent = confirm("Are you a student")
+console.log(isStudent)
+
+let age = prompt("Whats your age")
+console.log(age)
