cdcd7abb9a989252b8e474cb7049854a757e772f
Aniket-nitian <anik807621@gmail.com>
2024-10-13 14:15:19 +0530

    maths object completed

---
+++ 0.7 Math object/script.js
@@ -0,0 +1,9 @@
+
+const width = +prompt("Please Enter Rectangle Width");
+const height = +prompt("Please Enter Rectangle Height");
+
+console.log(width * height);
+document.write(`Rectangle Area: ${width * height}`); 
+// answere will de writen on top of the page
+
+console.log(Math.random()*(20-10)+10)
