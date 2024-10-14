f2eeb74a1d7ad238b303445b7f7b7116368913ba
Aniket-nitian <anik807621@gmail.com>
2024-10-14 12:10:38 +0530

    object basic completed

---
+++ 1.3 Object/script.js
@@ -0,0 +1,27 @@
+const user ={
+  Name: 'Aniket',  //Key-value pair
+  age: 21,
+  Status: 'Student',
+  'last-name': 'chauhan',
+  address: {
+    city: 'Muz',
+    pin: 842002
+  }
+}
+
+console.log(user.Name)
+console.log(user['Name'])
+console.log(user['age'])
+console.log(user['last-name'])
+console.log(user.address.pin)
+
+const Name = 'Aniket'
+
+const username1 = ''
+const username2 =''
+
+const user1 = {}
+const user2 = {}
+
+user1.age = 25   //adding key-value pair object       
+console.log(user1)
