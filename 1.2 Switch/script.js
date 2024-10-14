423cfe6f960a5258432a78a6f4c5df07e24a6311
Aniket-nitian <anik807621@gmail.com>
2024-10-14 11:00:03 +0530

    Switch statement completed

---
+++ 1.2 Switch/script.js
@@ -0,0 +1,25 @@
+const grade = prompt('Enter your grade: ')
+ const Grade = grade.toUpperCase()
+
+ console.log(`Your marks is ${Grade}`)
+switch(Grade){
+  case 'A':
+    console.log('marks > 80')
+      break
+    case 'B':
+    console.log('marks > 70')
+      break
+    case 'C':
+    console.log('marks > 60')
+      break
+    case 'D':
+    console.log('marks > 50')
+      break
+    case 'E':
+    console.log('marks > 40')
+      break
+    case 'F':
+    console.log('Better luck next time')
+    default:
+      console.log('Your are not a student')
+}
