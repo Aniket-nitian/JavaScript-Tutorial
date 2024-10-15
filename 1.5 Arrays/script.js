a2aa959364dc15b621acecf6c9f61331c77c321e
Aniket-nitian <anik807621@gmail.com>
2024-10-15 10:11:03 +0530

    Basic Array completed

---
+++ 1.5 Arrays/script.js
@@ -0,0 +1,24 @@
+const fruits = ['Apple','banana','Orange']
+
+console.log(fruits.length)  //Array length
+
+console.log(fruits)
+console.log( typeof fruits)
+
+console.log(fruits[2])   //viewing
+
+fruits[2]='Guava'    //changing
+console.log(fruits)
+
+fruits[fruits.length] = 'Grapes'  //adding
+fruits.push('Watermelon','coconut')  //adding using push
+
+console.log(fruits)
+
+
+
+console.log(fruits.length)
+
+const arrayObject = ['Aniket',21,{Class: 1,place: 'Muz'}]   //Object in array
+
+console.log(arrayObject.length)
