939936479280b0ebc6d19f005f3934870008aced
Aniket-nitian <anik807621@gmail.com>
2024-10-18 11:44:54 +0530

    while loop completed

---
+++ 2.1 While loop/script.js
@@ -0,0 +1,14 @@
+
+let num = 1
+while (num <= 10) {
+  console.log(num)
+  num++
+}
+
+let name = ['Aniket', 'sahil', 'Utkarsh', 'shivam', 'vivek', 'pushkar', 'peeyush']
+
+let i = 0
+while (i < name.length) {
+  console.log(`${i + 1}. = ${name[i]}`)
+
+}
