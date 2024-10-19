7739fb7ec7b420a7d180b1d35ae90a526ea53f53
Aniket-nitian <anik807621@gmail.com>
2024-10-19 18:58:39 +0530

    Hoisting completed

---
+++ 2.8 Hoisting/script.js
@@ -0,0 +1,17 @@
+console.log(user)
+
+var user = 'ani'
+
+
+history()
+
+function history(){
+  console.log('I love india')
+}     //function declaration / Function definition
+
+sayHi()
+
+var sayHi = function (){
+  console.log("Hiii");
+}      // function expression / Anonymous function / Function definition
+
