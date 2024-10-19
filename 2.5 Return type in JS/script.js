1c0991b5ae3f8889eba6f5c2374444eb7af7b1e9
Aniket-nitian <anik807621@gmail.com>
2024-10-19 10:44:22 +0530

    Return type completed

---
+++ 2.5 Return type in JS/script.js
@@ -0,0 +1,18 @@
+function Add(){
+  //console.log(5+6);
+  return 5+6
+}
+
+
+let x = Add()
+console.log(x)
+
+//let y
+function sub(a,b){
+ var y = b-a
+  return y
+}
+
+let z = sub(Add(),20-1)
+console.log(z)
+
