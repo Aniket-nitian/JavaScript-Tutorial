9e94840610abf42816f21a698f385a8cf48d28a9
Aniket-nitian <anik807621@gmail.com>
2024-10-19 19:20:46 +0530

    scope completed

---
+++ 2.9 Scope in JS/script.js
@@ -0,0 +1,22 @@
+const username = 'Aniket'
+var userAge = 25
+var a = 50
+
+function add(){
+  const x = 5
+  const y = 8
+  console.log(x+y)
+  console.log(username)
+}
+
+function subtract(){
+  const x = 15
+  const y = 18
+  console.log(x-y)
+  console.log(username)
+}
+
+add()
+subtract()
+
+
