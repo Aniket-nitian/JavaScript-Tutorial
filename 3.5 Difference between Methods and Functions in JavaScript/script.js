3213663f37e2a002fe939411286c5f124d080e6a
Aniket-nitian <anik807621@gmail.com>
2024-10-23 20:53:26 +0530

    function and method completed

---
+++ 3.5 Difference between Methods and Functions in JavaScript/script.js
@@ -0,0 +1,17 @@
+function add(){
+  console.log(2+4)
+}                   //This is a function
+
+const math = {
+ 
+
+ substract: function sub(){
+    console.log(3-5);
+  },
+
+  cube(){
+    return 3*3
+  }
+
+}               //this is method(function in a object)
+
