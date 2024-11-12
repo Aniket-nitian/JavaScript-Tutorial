0d6b0eb7c0e3836183937d9d6c06db9d75aef366
Aniket-nitian <anik807621@gmail.com>
2024-11-12 21:38:00 +0530

    Arguments completed

--- 4.2 Arguments Keyword in JavaScrip/script.js
+++ 4.2 Arguments Keyword in JavaScrip/script.js
@@ -23,7 +23,24 @@ const adddd =()=>{
 } 
 adddd(12,3,1,4)    //It will give error,therfore use nums, or use function instead of anonoumous function
 
-const addd = (...nums) => {
+const addd = (...nums) => {   //This is rest parameter , it is different from spread op, it gives data in the form of an array
+//Spread(Phailana), rest(Iktha)/Bachi hui values
+
+const nums1 = [1, 2, 3, 4]
+
+function add(a, b, c, ...nums) {  //rest parameter(always at last)
+    console.log(a, b, c);
+    console.log('nums:', nums);
+    let sum = 0
+    for(let i = 0; i < nums.length; i++) {
+        sum = sum + nums[i]
+    }
+    return sum
+}
+console.log(add(...nums1))
+
+
+
   let sum = 0
   for(let i = 0;i < nums.length;i++){
     sum += nums[i]
