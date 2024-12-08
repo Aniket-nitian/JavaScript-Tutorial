1dc0f2a5071c2391477d6e843eaca0fec0d3e598
Aniket-nitian <anik807621@gmail.com>
2024-12-08 11:07:35 +0530

    Dom started

--- DOM/script.js
+++ DOM/script.js
@@ -1,1 +1,21 @@
 console.log(document)
+console.dir(document)  //showing in a object format
+
+// See parent and children of each element??
+
+console.log(document.children[0])
+console.dir(document.children[0])
+
+console.log(document.children[0].children[0])
+console.dir(document.body)
+console.dir(document.body.children[2])
+
+//changing heading
+
+           //  document.body.children[0].innerHTML = 'Aniket'
+           //    document.body.children[0].innerText = 'Aniket'
+        
+
+// const change = document.body.children[1]
+
+// change.innerHTML = 'Frontent'
