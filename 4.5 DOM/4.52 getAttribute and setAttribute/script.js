f8bff20baf2adef074ff0df93c00b9e2a0f0e8b5
Aniket-nitian <anik807621@gmail.com>
2024-12-09 11:43:20 +0530

    SetAttibute and getAttribute completed.

--- 4.5 DOM/4.52 getAttribute and setAttribute/script.js
+++ 4.5 DOM/4.52 getAttribute and setAttribute/script.js
@@ -1,12 +1,12 @@
-document.querySelector('[procodrr]').attributes.procodrr.value  --> 'anurag'    //accessing value
- //or
+// document.querySelector('[procodrr]').attributes.procodrr.value  --> 'anurag'    //accessing value
+//  //or
 
- document.querySelector('[procodrr]').getAttribute('procodrr') --> 'anurag'
+//  document.querySelector('[procodrr]').getAttribute('procodrr') --> 'anurag'
 
- document.querySelector('h1')  --> <h1 class='css-image heading'>....</h1>
-document.querySelector('h1').getAttribute('class')  -->'ss-image heading'
+//  document.querySelector('h1')  --> <h1 class='css-image heading'>....</h1>
+// document.querySelector('h1').getAttribute('class')  -->'ss-image heading'
 
 
-document.querySelector('h1').setAttribute('attribue','value')
+// document.querySelector('h1').setAttribute('attribue','value')
 
-document.querySelector('h1').setAttribute('id','heading')
+// document.querySelector('h1').setAttribute('id','heading')
