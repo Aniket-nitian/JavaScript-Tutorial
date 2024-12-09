e35ad39ea9fb7eafca44b4498c52b3d148bb9c31
Aniket-nitian <anik807621@gmail.com>
2024-12-09 11:09:28 +0530

    Difference between innerText and textContent completed

---
+++ DOM/difference between innerText and innerContent/script.js
@@ -0,0 +1,13 @@
+const h1 = document.querySelector('h1')
+const paragraph = document.querySelector('p')
+
+console.log(paragraph.innerHTML);
+
+ paragraph.innerHTML = '<h4>Hiii</h4>'
+
+
+//  innerText --> will show only things which are about to show on a page if.e no extra spaces...h1.ATTRIBUTE_NODE.toExponential
+
+//  textContent --> it shows all this which are written in a code even extra spaces as WEBGL_lose_context...h1.ATTRIBUTE_NODE.toExponential
+
+// innerHTML --> shows tags as well...
