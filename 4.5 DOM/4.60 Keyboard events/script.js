56c53ddab741050dd718feff3c2078364f04010f
Aniket-nitian <anik807621@gmail.com>
2024-12-12 16:06:11 +0530

    Keyboard events completed

---
+++ 4.5 DOM/4.60 Keyboard events/script.js
@@ -0,0 +1,17 @@
+// const h1 = document.querySelector('h1')
+// const input = document.querySelector('input')
+
+// window.addEventListener('keypress',(e)=>{
+// console.log('value: ',e.key)
+// console.log('code: ',e.code)
+// })
+
+// window.addEventListener('keyup',(e)=>{  //only work when we press keyboard and lift our hand
+//   console.log('value: ',e.key)
+//   console.log('code: ',e.code)
+//   })
+
+  window.addEventListener('keydown',(e)=>{
+    console.log('value: ',e.key)
+    console.log('code: ',e.code)
+    })
