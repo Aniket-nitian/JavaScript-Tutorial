fee55b1d9598499cc1825c2a6275272c7be8a120
Aniket-nitian <anik807621@gmail.com>
2024-12-27 13:06:31 +0530

    Learning API started

--- 4.8 API/script.js
+++ 4.8 API/script.js
@@ -1,5 +1,16 @@
 //json ia a valid object in js.
+//the response from the server is sent in JSON(js object notation) format
 
-fetch('https://jsonplaceholder.typicode.com/todos/1')
+const img = document.querySelector('img');
+const button = document.querySelector('button');
+
+
+
+button.addEventListener('click',(event) => {
+      fetch('https://dog.ceo/api/breeds/image/random')
       .then(response => response.json())
-      .then(json => console.log(json))
+      .then(json => {
+            img.src = json.message;
+      })
+      
+})
